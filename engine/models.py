0033e38b18bc41f9cf946ac30432d903108dd06d
unknown <mukulbarai82@gmail.com>
2019-07-29 17:07:31 +0600

    changed indentation

--- engine/models.py
+++ engine/models.py
@@ -1,24 +1,22 @@
 from django.db import models
 
 class Page(models.Model):
-  title = models.CharField(max_length=200)
-  url = models.CharField(max_length=500)
-  description = models.CharField(max_length=1000)
-  keywords = models.CharField(max_length=1000)
-  crawled = models.IntegerField(default=0)
-  broken = models.BooleanField(default=0)
-  clicks = models.IntegerField(default=0)
-  def __str__(self): return self.url
-#end page
+    title = models.CharField(max_length=200)
+    url = models.CharField(max_length=500)
+    description = models.CharField(max_length=1000)
+    keywords = models.CharField(max_length=1000)
+    crawled = models.IntegerField(default=0)
+    broken = models.BooleanField(default=0)
+    clicks = models.IntegerField(default=0)
+    def __str__(self): return self.url
 
 
 class Image(models.Model):
-  url = models.CharField(max_length=500)
-  src = models.CharField(max_length=200)
-  title = models.CharField(max_length=200)
-  alt = models.CharField(max_length=200)
-  broken = models.BooleanField(default=0)
-  clicks = models.IntegerField(default=0)
-  def __str__(self): return self.src
-#end image
+    url = models.CharField(max_length=500)
+    src = models.CharField(max_length=200)
+    title = models.CharField(max_length=200)
+    alt = models.CharField(max_length=200)
+    broken = models.BooleanField(default=0)
+    clicks = models.IntegerField(default=0)
+    def __str__(self): return self.src
 
